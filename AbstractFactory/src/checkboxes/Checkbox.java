7cdacb89b0ca5b136d88fe52ae12a74b63a37187
TruongCongTri <44316735+TruongCongTri@users.noreply.github.com>
2021-07-13 11:18:18 +0700

    Upload AbstractFactory
    Abstract Factory codes and result image

---
+++ AbstractFactory/src/checkboxes/Checkbox.java
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package checkboxes;
+
+/**
+ * Checkboxes is the second product family. It has the same variants as buttons.
+ */
+public interface Checkbox {
+    void paint();
+}
---
+++ AbstractFactory/src/factories/GUIFactory.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package factories;
+
+import buttons.Button;
+import checkboxes.Checkbox;
+
+/**
+ * Abstract factory knows about all (abstract) product types.
+ */
+public interface GUIFactory {
+    Button createButton();
+    Checkbox createCheckbox();
+}

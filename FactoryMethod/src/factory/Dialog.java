b32a709deca082173d80ef23154b55a77841b4b9
TruongCongTri <44316735+TruongCongTri@users.noreply.github.com>
2021-07-13 11:06:27 +0700

    Upload FactoryMethod
    Factory Method codes and result image

---
+++ FactoryMethod/src/factory/Dialog.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package factory;
+
+import buttons.Button;
+
+/**
+ * Base factory class. Note that "factory" is merely a role for the class. It
+ * should have some core business logic which needs different products to be
+ * created.
+ */
+public abstract class Dialog {
+    
+    public void renderWindow() {
+        // ... other code ...
+
+        Button okButton = createButton();
+        okButton.render();
+    }
+
+    /**
+     * Subclasses will override this method in order to create specific button
+     * objects.
+     */
+    public abstract Button createButton();
+}
---
+++ FactoryMethod/src/factory/HtmlDialog.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package factory;
+
+import buttons.Button;
+import buttons.HtmlButton;
+
+/**
+ * HTML Dialog will produce HTML buttons.
+ */
+public class HtmlDialog extends Dialog{
+    
+    @Override
+    public Button createButton() {
+        return new HtmlButton();
+    }
+}

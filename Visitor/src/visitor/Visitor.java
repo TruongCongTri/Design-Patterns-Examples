7191b9ea88ba3ac953031d67b5f4d26b3d745384
TruongCongTri <44316735+TruongCongTri@users.noreply.github.com>
2021-07-14 10:07:20 +0700

    Upload Visitor
    Visitor codes and result image

---
+++ Visitor/src/visitor/Visitor.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package visitor;
+
+import shapes.Circle;
+import shapes.CompoundShape;
+import shapes.Dot;
+import shapes.Rectangle;
+
+/**
+ *
+ * @author congt
+ */
+public interface Visitor {
+    String visitDot(Dot dot);
+
+    String visitCircle(Circle circle);
+
+    String visitRectangle(Rectangle rectangle);
+
+    String visitCompoundGraphic(CompoundShape cg);
+}
